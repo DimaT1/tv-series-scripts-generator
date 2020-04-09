470b64705f04b14abcdb99fed9df54fb7445137a
DimaT1 <dymych99@gmail.com>
2020-04-10 02:46:53 +0300

    add documentation generator

---
+++ generate_docs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+mds="./corpus_preprocessing/corpus_description.md
+./corpus_preprocessing/preprocessing.md
+./text_generation/text_generation.md"
+
+mkdir -p .build
+echo '' > ./.build/full.md
+
+while IFS= read -r line; do
+    cat "$line" >> ./.build/full.md
+    printf "\n" >> ./.build/full.md
+done <<< "$mds"
+
+pandoc -s -o ./docs/documentation.pdf ./.build/full.md
